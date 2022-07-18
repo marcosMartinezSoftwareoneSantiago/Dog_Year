d48fcbf0c0f415ab5d8b2de110de61f492b5ec49
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-07-18 11:33:21 +0200

    a dog commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var myAge = 58;
+//that's my age
+var earlyYears = 2;
+//first years of a dog
+earlyYears *= 10.5;
+var largeYears = myAge - 2;
+largeYears = largeYears * 4;
+console.log(earlyYears + ' '+largeYears);
+myAgeInDogYears = earlyYears + largeYears;
+//my age if I were a dog
+const myName = 'Marcos'.toLowerCase();
+//that's my name
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+//that is the result
